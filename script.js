1dcc70cb3e05507e239c0b45a0e3dbbd8f9aba18
3boodyy96 <abdosee517@gmail.com>
2026-01-16 18:10:35 +0300

    editing js code and adding checkboxes to html

---
+++ script.js
@@ -0,0 +1,19 @@
+// select the elements
+const todoInput = document.getElementById('todo-input');
+const addTodoBtn = document.getElementById('add-todo-btn');
+const todoList = document.getElementById('todo-list');
+
+// function to add a new todo
+
+
+addTodoBtn.addEventListener('click', () => {
+    let todoText = todoInput.value.trim();
+    if (todoText !== "") {
+        todoList.innerHTML += `
+            <li class="flex items-center gap-2 p-2">
+                <input type="checkbox" class="w-4 h-4">
+                <span>${todoText}</span>
+            </li>`;
+        todoInput.value = "";
+    }
+});
